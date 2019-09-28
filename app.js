a92da64b65fe52d78a34dcbcbc326a56dff51b13
dziobakwszafie <dziobakwszafie@gmail.com>
2019-09-28 12:56:55 +0200

    start v.4.1.1 one site

--- app.js
+++ app.js
@@ -20,32 +20,3 @@ const navslide = () => {
 }
 
 navslide();
-
-const kaszana = document.querySelector('#people').nodeValue;
-console.log(kaszana)
-
-function calc() {
-    const weight = document.querySelector
-}
-
-/*
-wartości z selektora przyporządkowywujemy wartości wartosciom w tablicy 
-
-petla przyporz
-
-
-var e = document.getElementById("dupa2");
-e.addEventListener("change", function () {
-var strUser = e.options[e.selectedIndex].value;
-document.getElementById("dupa").innerHTML = strUser;
-})
-
-
-tablica 1 z masą 
-tablica 2 ze wzrostem 
-bierzeym największą wartość 
-korygujemy tę wartosć o wiek 
-tworzymy tablice dwuwymiarową 
-rzad wybieram z tej wiekszej z 2 wartosci 
-kolumne na podstawie buta 
-*/
